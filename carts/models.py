42700dd9490ed4d4e3ef7935cde70b7deeaf866b
byoungjupark <byoungju1012@gmail.com>
2021-08-04 18:22:18 +0900

    Add: carts/models  사이즈 필드 추가

--- carts/models.py
+++ carts/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Cart(models.Model):
     user    = models.ForeignKey('users.User', on_delete=models.CASCADE)
     product = models.ForeignKey('products.Product', on_delete=models.CASCADE)
+    size    = models.ForeignKey('products.Size', on_delete=models.CASCADE)
     count   = models.PositiveIntegerField()
 
     class Meta:
