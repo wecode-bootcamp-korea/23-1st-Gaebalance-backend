7f210f9d3152e7bc903062cd86c05e7869c6c641
JINSU <songjinsu@MacBook-Pro.local>
2021-08-05 15:53:35 +0900

    Modify: 제품 상세 이미지 리스트 추가

--- products/views.py
+++ products/views.py
@@ -1,25 +1,32 @@
 from django.views import View
 from django.http  import JsonResponse
 
-from products.models import Product
+from products.models import Product, DetailImage
 
 class ProductDetailView(View):
     def get(self, request, product_id):
         try:
             product = Product.objects.get(id=product_id)
+            images  = DetailImage.objects.filter(product=product_id)
+
+            image_list = []
+
+            for image in images:
+                image_list.append(image.image_url)
 
             response = {
-                "id" : product.id,
-                "name" : product.name,
-                "price" : int(product.price),
-                "style_code" : product.style_code,
-                "origin" : product.origin,
+                "id"               : product.id,
+                "name"             : product.name,
+                "price"            : int(product.price),
+                "style_code"       : product.style_code,
+                "origin"           : product.origin,
                 "manufacture_date" : product.manufacture_date,
-                "description" : product.description,
-                "image_url" : product.image_url,
-                "color" : product.color.name,
-                "sub_category" : product.sub_category.sub_category,
-                "category" : product.sub_category.category.category,
+                "description"      : product.description,
+                "image_url"        : product.image_url,
+                "image_list"       : image_list,
+                "color"            : product.color.name,
+                "sub_category"     : product.sub_category.sub_category,
+                "category"         : product.sub_category.category.category,
             }
 
             return JsonResponse({"response": response}, status=200)
