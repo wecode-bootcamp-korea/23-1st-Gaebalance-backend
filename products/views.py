59a764a1c9b7e7b6393475d8f2824e9184a61970
JINSU <songjinsu@MacBook-Pro.local>
2021-08-05 15:34:35 +0900

    Add: View/상품 상세 정보 조회

--- products/views.py
+++ products/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.views import View
+from django.http  import JsonResponse
 
-# Create your views here.
+from products.models import Product
+
+class ProductDetailView(View):
+    def get(self, request, product_id):
+        try:
+            product = Product.objects.get(id=product_id)
+
+            response = {
+                "id" : product.id,
+                "name" : product.name,
+                "price" : int(product.price),
+                "style_code" : product.style_code,
+                "origin" : product.origin,
+                "manufacture_date" : product.manufacture_date,
+                "description" : product.description,
+                "image_url" : product.image_url,
+                "color" : product.color.name,
+                "sub_category" : product.sub_category.sub_category,
+                "category" : product.sub_category.category.category,
+            }
+
+            return JsonResponse({"response": response}, status=200)
+        
+        except Product.DoesNotExist:
+            return JsonResponse({"message":"PRODUCT_DOES_NOT_EXIST"}, status=400)
+
+        except KeyError:
+            return JsonResponse({"message":"KEY_ERROR"}, status=400)
