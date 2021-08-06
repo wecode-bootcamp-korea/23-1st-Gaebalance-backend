e208547941584d8989941df5d0b09079f575daf3
JINSU <songjinsu@MacBook-Pro.local>
2021-08-06 14:24:49 +0900

    Modify: 리뷰 수정사항 반영

--- gaebalance/urls.py
+++ gaebalance/urls.py
@@ -17,5 +17,5 @@ Including another URLconf
 from django.urls import path, include
 
 urlpatterns = [
-    path('product', include('products.urls')),
+    path('products', include('products.urls')),
 ]
--- products/views.py
+++ products/views.py
@@ -1,38 +1,30 @@
 from django.views import View
 from django.http  import JsonResponse
 
-from products.models import Product, DetailImage
+from products.models import Product
 
-class ProductDetailView(View):
+class ProductView(View):
     def get(self, request, product_id):
-        try:
-            product = Product.objects.get(id=product_id)
-            images  = DetailImage.objects.filter(product=product_id)
-
-            image_list = []
-
-            for image in images:
-                image_list.append(image.image_url)
-
-            response = {
-                "id"               : product.id,
-                "name"             : product.name,
-                "price"            : int(product.price),
-                "style_code"       : product.style_code,
-                "origin"           : product.origin,
-                "manufacture_date" : product.manufacture_date,
-                "description"      : product.description,
-                "image_url"        : product.image_url,
-                "image_list"       : image_list,
-                "color"            : product.color.name,
-                "sub_category"     : product.sub_category.sub_category,
-                "category"         : product.sub_category.category.category,
-            }
-
-            return JsonResponse({"response": response}, status=200)
-        
-        except Product.DoesNotExist:
+        if not Product.objects.filter(id=product_id).exists():
             return JsonResponse({"message":"PRODUCT_DOES_NOT_EXIST"}, status=400)
 
-        except KeyError:
-            return JsonResponse({"message":"KEY_ERROR"}, status=400)
+        product = Product.objects.get(id=product_id)
+
+        image_list = [image.image_url for image in product.detailimage_set.all()]
+
+        response = {
+            "id"               : product.id,
+            "name"             : product.name,
+            "price"            : int(product.price),
+            "style_code"       : product.style_code,
+            "origin"           : product.origin,
+            "manufacture_date" : product.manufacture_date,
+            "description"      : product.description,
+            "image_url"        : product.image_url,
+            "image_list"       : image_list,
+            "color"            : product.color.name,
+            "sub_category"     : product.sub_category.sub_category,
+            "category"         : product.sub_category.category.category,
+        }
+
+        return JsonResponse({"response": response}, status=200)
