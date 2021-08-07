b0b7cfb058f81323b544b4e111ac321f7d6b1ad4
byoungjupark <byoungju1012@gmail.com>
2021-08-07 14:57:09 +0900

    Add: 장바구니GET API

--- carts/views.py
+++ carts/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import JsonResponse
+from django.views import View
+
+from carts.models import Cart
+from users.models import User
+from products.models import Product, Size
+
+class CartView(View):
+    @LoginDecorator
+    def get(self, request):
+        if not Cart.objects.filter(user_id = request.user.id).exists():
+            return JsonResponse({"message":"CART_DOES_NOT_EXIST"}, status = 400)
+            
+        carts       = Cart.objects.filter(user_id = request.user.id)
+        total_price = 0
+        response    = []
+
+        for cart in carts:
+            response.append({
+                "name"  : cart.product.name,
+                "image" : cart.product.image_url,
+                "color" : cart.product.color.name,
+                "size"  : cart.size.name,
+                "count" : cart.count,
+                "price" : cart.count * cart.product.price
+            })
+            total_price += cart.count * cart.product.price
+            
+        response.append({"total_price" : total_price})
+
+        return JsonResponse({"response":response}, status = 200)
