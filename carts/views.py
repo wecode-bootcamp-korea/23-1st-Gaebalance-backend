40157b9ab0998f849246c9e20b4acb7fd5b3ef4a
byoungjupark <byoungju1012@gmail.com>
2021-08-07 15:07:30 +0900

    Add: 장바구니 DELETE API

--- carts/views.py
+++ carts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import JsonResponse
+from django.views import View
+
+from carts.models import Cart
+from users.models import User
+from products.models import Product, Size
+
+class CartView(View):
+    @LoginDecorator
+    def delete(self, request):
+        cart = request.GET.getlist("id")
+
+        if not Cart.objects.filter(id__in = cart).exists():
+            return JsonResponse({"message":"CART_DOES_NOT_EXIST"}, status = 400)
+
+        Cart.objects.filter(id__in = cart).delete()
+
+        return JsonResponse({"message":"DELETED"}, status = 204)
