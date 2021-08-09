82315ddee75d131c20805be6945f4a4c1c998e56
byoungjupark <byoungju1012@gmail.com>
2021-08-09 18:23:29 +0900

    Modify: 장바구니 GET API (aggregate 적용)

--- carts/views.py
+++ carts/views.py
@@ -2,32 +2,28 @@ import json
 
 from django.http import JsonResponse
 from django.views import View
+from django.db.models import Sum, F
 
 from carts.models import Cart
 from users.models import User
 from products.models import Product, Size
+from users.utils import login_deco
 
 class CartView(View):
-    @LoginDecorator
-    def get(self, request):
-        if not Cart.objects.filter(user_id = request.user.id).exists():
-            return JsonResponse({"message":"CART_DOES_NOT_EXIST"}, status = 400)
-            
-        carts       = Cart.objects.filter(user_id = request.user.id)
-        total_price = 0
-        response    = []
+    @login_deco
+    def get(self, request):            
+        carts = Cart.objects.filter(user_id = 4)
+        total_price = carts.aggregate(price = Sum(F("count") * F("product__price")))
 
-        for cart in carts:
-            response.append({
-                "name"  : cart.product.name,
-                "image" : cart.product.image_url,
-                "color" : cart.product.color.name,
-                "size"  : cart.size.name,
-                "count" : cart.count,
-                "price" : cart.count * cart.product.price
-            })
-            total_price += cart.count * cart.product.price
-            
-        response.append({"total_price" : total_price})
+        response = [{
+            "name"  : cart.product.name,
+            "image" : cart.product.image_url,
+            "color" : cart.product.color.name,
+            "size"  : cart.size.name,
+            "count" : cart.count,
+            "price" : cart.count * cart.product.price}
+            for cart in carts]
+
+        response.append({"total_price" : total_price["price"]})
 
         return JsonResponse({"response":response}, status = 200)
