2ed03d2b3099a312963c5808aaae38c972d628fb
damdream <possible_11@naver.com>
2021-08-09 17:42:40 +0900

    [23기 김도담] 리뷰 반영

--- users/views.py
+++ users/views.py
@@ -12,18 +12,19 @@ email_ragular    = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
 password_regular = re.compile('^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,12}$')
 
 class JoinView(View):
-    def post(self,request):
-        try:
-            data =json.loads(request.body)
-            bcrypt_password = bcrypt.hashpw(data["password"].encode("utf-8"),bcrypt.gensalt()). decode("utf-8")
+	def post(self,request):
+		try:
+			data = json.loads(request.body)
            
-            if (User.objects.filter(email=data["email"]).exists()) or (User.objects.filter(phone_number=data['phone_number']).exists()):
-                return JsonResponse ({"MESSAGE": "EXIST_DATA"}, status = 400)
-
-            if (not email_ragular.match(data["email"])) or (not password_regular.match(data["password"])):
-                return JsonResponse ({"MESSAGE":"INVALID_FORMAT"}, status = 400)	
-  
-       	    user = User.objects.create(
+			if (not email_ragular.match(data["email"])) or (not password_regular.match(data["password"])):
+				return JsonResponse ({"MESSAGE":"INVALID_FORMAT"}, status = 400)
+				
+			if (User.objects.filter(email=data["email"]).exists()) or (User.objects.filter(phone_number=data['phone_number']).exists()):
+				return JsonResponse ({"MESSAGE": "EXIST_DATA"}, status = 400)
+				
+			bcrypt_password = bcrypt.hashpw(data["password"].encode("utf-8"),bcrypt.gensalt()). decode("utf-8")	
+			
+			user = User.objects.create(
 				name         = data["name"],
 				gender       = data["gender"],
 				birth_date   = data["birth_date"],
@@ -32,14 +33,13 @@ class JoinView(View):
 				password     = bcrypt_password,
 				address      = data["address"],
 				size         = Size.objects.get(id=data["size_id"])
-			)  
-		    
-            UserColor.objects.bulk_create([ UserColor(user=User.objects.get(id=user.id), color=Color.objects.get(id=color_id)) for color_id in data["colors_id"]])
-
-            return JsonResponse ({"MESSAGE":"CREATED"}, status = 201)
-				
-        except KeyError:
-       	    return JsonResponse ({"MESSAGE":"KEY_ERROR"}, status = 400)
+			)
+			
+			UserColor.objects.bulk_create([ UserColor(user=User.objects.get(id=user.id), color=Color.objects.get(id=color_id)) for color_id in data["colors_id"]])
+			return JsonResponse ({"MESSAGE":"CREATED"}, status = 201)
+		
+		except KeyError:
+			return JsonResponse({"MESSAGE":"KEY_ERROR"}, status = 400)
 
 class LoginView(View):
 	def post(self,request):
