94d4b0b2ff2bfc4249990fc5da79af042147c9c2
damdream <possible_11@naver.com>
2021-08-12 03:48:20 +0900

    add: 리뷰 GET, DELETE, PATCH작성

--- gaebalance/urls.py
+++ gaebalance/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path ('users', include('users.urls')),
     path('products', include('products.urls')),
     path('carts', include('carts.urls')),
+    path('reviews', include('reviews.urls')),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http      import JsonResponse, request
+from django.views     import View
+from reviews.models   import Review
+from users.models     import User
+from products.models  import Product
+from users.utils      import login_decorator
+
+class ReviewView(View):
+    def get(self,request, review_id):
+        reviews = Review.objects.all()
+        result = []
+
+        if not Review.objects.filter(id=review_id).exists():
+            return JsonResponse ({"MESSAGE": "REVIEW_DOES_NOT_EXISTS"}, status = 400)   
+ 
+        result = [
+            {
+                "user"            : review.user.id,
+                "product_id"      : review.product_id ,
+                "create_at"       : review.created_at,
+                "size_rating"     : review.size_rating,
+                "color_rating"    : review.color_rating,
+                "delivery_rating" : review.delivery_rating,
+                "title"           : review.title,
+                "comment"         : review.comment,
+
+            } for review in reviews]
+
+        return JsonResponse ({"result": result}, status = 200)
+
+    @login_decorator
+    def delete (self,request,review_id):
+
+        if not Review.objects.filter(id=review_id).exists():
+            return JsonResponse({"MESSAGE": "NO_REVIEWS"}, status = 400)
+
+        if Review.objects.get(id=review_id) != request.user :
+            return JsonResponse({"MESSAGE": "UNAUTHROIZED"}, status = 400)
+
+        Review.objects.filter(id=review_id).delete()
+
+        return JsonResponse ({"MESSAGE" : "DELETE"}, status = 204)    
+
+    @login_decorator
+    def patch (self,request, review_id):
+        try:
+            data = json.loads(request.body)
+
+            if not Review.objects.filter(id=review_id).exists():
+                return JsonResponse({"MESSAGE": "NO_REVIEWS"}, status = 400)
+
+            if Review.objects.get(id=review_id) != request.user :
+                return JsonResponse({"MESSAGE": "UNAUTHROIZED"}, status = 400)
+
+            Review.objects.filter(id= data["review_id"]).update(data["title"], data["comment"])
+
+            return JsonResponse ({"MESSAGE" : "UPDATE"}, status = 200)    
+
+        except KeyError:
+            return JsonResponse ({"MESSAGE":"KEY_ERROR"},status = 400)    
